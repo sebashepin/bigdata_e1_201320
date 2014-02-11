8f293966c3d2b792840251d44f1c679fc629454e
Sebastian <sebashepin@gmail.com>
2014-02-11 15:17:22 -0500

    Initial main crawler commit

---
+++ crawl_institucional/crawl_institucional/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for crawl_institucional project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'crawl_institucional'
+
+SPIDER_MODULES = ['crawl_institucional.spiders']
+NEWSPIDER_MODULE = 'crawl_institucional.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'crawl_institucional (+http://www.yourdomain.com)'
