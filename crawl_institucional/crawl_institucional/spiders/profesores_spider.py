a37892c9a1539a5337ae6e88a9aa707e97c2f188
David Derby <dderby@server.fake>
2014-02-14 19:17:22 -0500

    Extract department URLs from university home page

---
+++ crawl_institucional/crawl_institucional/spiders/profesores_spider.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from scrapy.spider import Spider
+from scrapy.selector import Selector
+from crawl_institucional.items import ProfesorItem
+from scrapy.http import Request
+
+class ProfesoresSpider(Spider):
+    name = "profesores"
+    allowed_domains = ["uniandes.edu.co"]
+    start_urls = ["http://uniandes.edu.co/"]
+
+    def parse(self, response):
+        sel = Selector(response)
+	sitios = sel.xpath("//div[@id='facultad']//li[@class='submenu2']/ul/li/a")
+	enlaces = []
+	for sitio in sitios:
+	    titulo = sitio.xpath('text()').extract()
+	    enlaces.append(sitio.xpath('@href').extract())
+	enlacesVisitados = []
+	for enlace in enlaces:
+	    enlacesVisitados.append(enlace)
+	    print enlace
+	    #yield Request(enlace, self.parse_departamento)
+
+    def parse_departamento(self, response):
+        sel = Selector(response)
+	enlanceProfesores = sel.xpath("//a/text()='Profesores de planta'")
+	print enlanceProfesores
